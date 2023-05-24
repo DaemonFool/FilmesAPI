2749016e8653ef67d0df2c7facddc787168586fc
Marcus Duarte <marcus.duarte@visagio.com>
2023-05-24 11:25:49 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController
+{
+    public void adicionaFilme()
+    {
+
+    }
+}
